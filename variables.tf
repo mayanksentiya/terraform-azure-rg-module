83dc3cfbe0e617384387d64fa4b487a6a85cefce
mayanksentiya <31043590+mayanksentiya@users.noreply.github.com>
2024-04-09 23:30:01 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "name" {
+    type = string
+    description = "(optional) describe your variable"
+    default = "RG-1"
+}
+
+variable "location" {
+    type = string
+    description = "(optional) describe your variable"
+    default = "EAST-US"
+}
